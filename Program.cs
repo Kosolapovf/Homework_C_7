8a2fa41a4f419fadb560cadf06daf2650c4770de
Egor <EgorrKosolapoff@ya.ru>
2023-08-09 16:49:58 +0700

    second commit

--- Program.cs
+++ Program.cs
@@ -110,5 +110,5 @@ Console.Write("введите размер матрицы: ");
 int[] size = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
 int[,] matrix = new int[size[0], size[1]];
 InputArray(matrix);
-ReleaseArray(matrix);
- */
+ReleaseArray(matrix); */
+
